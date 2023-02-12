c9b70fbb25cfe4f97a8b7c9a36427c55ac216280
MTema.Extern <mtema.extern@1und1.net>
2023-02-12 13:58:26 +0100

    DX-1000 First commit

--- src/handler.js
+++ src/handler.js
@@ -1,16 +1,38 @@
-const process = require("process");
-//const convert = require('xml-js');
+const xmljs = require('xml-js');
 
 module.exports = async function (context) {
+
+    const {request} = context
+
+    const from = request.headers['x-fission-params-from']
+    const to = request.headers['x-fission-params-to']
+
+    const configurations = {
+        'xml-json': {
+            handler: xmljs.xml2json,
+            config: {compact: true, spaces: 4},
+            payload: request.body,
+            header: {
+                "Content-Type": "application/json"
+            }
+        },
+        'json-xml': {
+            handler: xmljs.json2xml,
+            config: {compact: true, spaces: 4, ignoreComment: true},
+            payload: request.body,
+            header: {
+                "Content-Type": "plain/html"
+            }
+        }
+    }
+
+    const configuration = configurations[from + '-' + to]
+
+    const {handler, config, payload, header} = configuration;
+
     return {
         status: 200,
-        body: {
-            foo: new Date(),
-            BODY_PARSER_LIMIT: process.env.BODY_PARSER_LIMIT,
-            ENV: process.env
-        },
-        header: {
-            "Content-Type": "application/json"
-        }
+        body: handler(payload, config),
+        header
     };
 };
